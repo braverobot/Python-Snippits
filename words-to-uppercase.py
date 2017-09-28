a2dce48f6bd44235da7b1c10cfab67bdeba346b0
Brian Sudol <briansudol@gmail.com>
2017-09-27 19:32:00 -0500

    Update words-to-uppercase.py
    adding try / except

--- words-to-uppercase.py
+++ words-to-uppercase.py
@@ -1,9 +1,13 @@
 # This file reads in a file name and changes all characters to upper-case
-# 
 #
 fname = input("Enter file name: ")
-fh = open(fname)
-
+### Check to make sure the file opens properly
+try:
+    fh = open(fname)
+except:
+    print("There was an error in your filename. Please check the name and try again")
+    quit()
+# Read in each line, strip the newline, and change all lines to caps
 for line in fh:
     line=line.strip()
-    print(line.upper())
+    print(line.upper())                     
