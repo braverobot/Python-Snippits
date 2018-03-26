425239f1d89cfff20db2ec33f5114df50cf8d653
Brian Sudol <briansudol@gmail.com>
2018-03-26 07:21:07 -0500

    Update greatest-smallest.py

--- greatest-smallest.py
+++ greatest-smallest.py
@@ -1,12 +1,9 @@
-#####################################
-## Author: Brian Sudol
-## Date: 9-23-17
+
 ## Purpose:
 ##  This program reads input from user. If input is "done", then end the program and print the min and max numbers entered
 ##  If the input is a number, store the integer as largest variable and smallest variable and repromput user. Continue to
 ##  prompt user for input and evaluate what largest and smallest variables were, until "done" is entered. Add an exception
 ##  that says if the input is not an integer, and the input is not equal to "done", throw exeption error and reprompt for input.
-#####################################
 largest = None
 smallest = None
 while True:
