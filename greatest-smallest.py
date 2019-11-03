35b05fbdb7409d506c2c2ec1036397132bfa9941
heartwood <heartwood@heartwoods-Mac-mini.local>
2019-11-03 11:27:45 -0600

    linting

--- greatest-smallest.py
+++ greatest-smallest.py
@@ -1,28 +1,32 @@
-#  This program reads input from user. If input is "done", then end the program and print the min and max numbers entered
-#  If the input is a number, store the integer as largest variable and smallest variable and repromput user. Continue to
-#  prompt user for input and evaluate what largest and smallest variables were, until "done" is entered. Add an exception
-#  that says if the input is not an integer, and the input is not equal to "done", throw exeption error and reprompt for input.
+"""This program reads input from user. If input is "done", then end the program and print the min
+and max numbers entered If the input is a number, store the integer as largest variable and
+smallest variable and repromput user. Continue to prompt user for input and evaluate what
+largest and smallest variables were, until "done" is entered. Add an exception that says if the
+input is not an integer, and the input is not equal to "done", throw exeption error and reprompt
+for input.
+"""
+
 largest = None
 smallest = None
 while True:
-    num= input("Enter a number:")
+    num = input("Enter a number ('done' to quit):")
     if num != "done":
         try:
-            check= int(num)
-        except:
-              print("Invalid input")
-              continue
+            check = int(num)
+        except ValueError:
+            print("Invalid Input")
+            continue
         else:
             num = int(num)
-            if largest == None:
+            if largest is None:
                 largest = int(num)
-            if smallest == None:
+            if smallest is None:
                 smallest = int(num)
             if num >= largest:
                 largest = int(num)
             if num <= smallest:
                 smallest = num
-    if num =="done":
+    if num == "done":
         print("Largest Number input is", largest)
         print("Smallest Number input is", smallest)
         break
